7e332c10680d45616e932b990338aac560015f19
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-04-02 18:24:22 -0300

    update
    #correções menores

--- src/PedraPapelOuTesoura/app/src/main/java/com/example/jhonatasrm/pedrapapeloutesoura/MainActivity.java
+++ src/PedraPapelOuTesoura/app/src/main/java/com/example/jhonatasrm/pedrapapeloutesoura/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.jhonatasrm.pedrapapeloutesoura;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,82 +14,77 @@ public class MainActivity extends AppCompatActivity {
     ImageButton jogoMaquina;
     int valorRandom;
     Random random;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        configuraçãoInicial();
         random = new Random();
-
-        resultado = findViewById(R.id.resultado);
-        jogoMaquina = findViewById(R.id.jogoMaquina);
-        resultadoVitorias  = findViewById(R.id.vitorias);
-        resultadoDerrotas = findViewById(R.id.derrotas);
-        resultadoEmpates = findViewById(R.id.empates);
-        resultado.setText(R.string.clique_para_comecar);
-
-        vitorias = 0;
-        derrotas = 0;
-        empates = 0;
     }
 
-    public  void jogadaMaquina(){
+    //jogada da máquina onde um valor random é atribuido a uma imagem
+    public void jogadaMaquina() {
         valorRandom = random.nextInt(3);
-        if(valorRandom == 0){
+        if (valorRandom == 0) {
             jogoMaquina.setImageResource(R.drawable.pedra);
-        }else if (valorRandom == 1){
+        } else if (valorRandom == 1) {
             jogoMaquina.setImageResource(R.drawable.papel);
-        }else{
+        } else {
             jogoMaquina.setImageResource(R.drawable.tesoura);
         }
     }
 
-    public  void jogaPapel(View view){
+    //caso clicado no papel compara com a jogada da máquina
+    public void jogaPapel(View view) {
         jogadaMaquina();
-        if(valorRandom == 0){
+        if (valorRandom == 0) {
             resultado.setText(R.string.voce_ganhou);
             vitorias = vitorias + 1;
-        }else if(valorRandom == 1){
+        } else if (valorRandom == 1) {
             resultado.setText(R.string.empate);
             empates = empates + 1;
-        }else{
+        } else {
             resultado.setText(R.string.voce_perdeu);
             derrotas = derrotas + 1;
         }
         mostraResultados();
     }
 
-    public  void jogaPedra(View view){
+    //caso clicado na pedra compara com a jogada da máquina
+    public void jogaPedra(View view) {
         jogadaMaquina();
-        if(valorRandom == 0){
+        if (valorRandom == 0) {
             resultado.setText(R.string.empate);
             empates = empates + 1;
-        }else if(valorRandom == 1){
+        } else if (valorRandom == 1) {
             resultado.setText(R.string.voce_perdeu);
             derrotas = derrotas + 1;
-        }else{
+        } else {
             resultado.setText(R.string.voce_ganhou);
             vitorias = vitorias + 1;
         }
         mostraResultados();
     }
 
-    public  void jogaTesoura(View view){
+    //caso clicado na tesoura compara com a jogada da máquina
+    public void jogaTesoura(View view) {
         jogadaMaquina();
-        if(valorRandom == 0){
+        if (valorRandom == 0) {
             resultado.setText(R.string.voce_perdeu);
             derrotas = derrotas + 1;
-        }else if(valorRandom == 1){
+        } else if (valorRandom == 1) {
             resultado.setText(R.string.voce_ganhou);
             vitorias = vitorias + 1;
-        }else{
+        } else {
             resultado.setText(R.string.empate);
             empates = empates + 1;
         }
         mostraResultados();
     }
 
-    public void mostraResultados(){
+    //apresenta o resultado de cada jogada
+    public void mostraResultados() {
         resultadoVitorias.setText(R.string.total_vitorias);
         resultadoVitorias.append(" " + Integer.toString(vitorias));
         resultadoDerrotas.setText(R.string.total_derrotas);
@@ -97,4 +93,17 @@ public class MainActivity extends AppCompatActivity {
         resultadoEmpates.append(" " + Integer.toString(empates));
     }
 
+    //prepara a tela para iniciar as jogadas
+    public void configuraçãoInicial() {
+        resultado = findViewById(R.id.resultado);
+        jogoMaquina = findViewById(R.id.jogoMaquina);
+        resultadoVitorias = findViewById(R.id.vitorias);
+        resultadoDerrotas = findViewById(R.id.derrotas);
+        resultadoEmpates = findViewById(R.id.empates);
+        resultado.setText(R.string.clique_para_comecar);
+
+        vitorias = 0;
+        derrotas = 0;
+        empates = 0;
+    }
 }
